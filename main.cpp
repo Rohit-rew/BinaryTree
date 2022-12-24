9357916de440194808e55319246212e25e93bb90
Rohit Kumar <rohit.rew@gmail.com>
2022-12-24 12:58:25 +0530

    traversals methods created

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 # include <iostream>
-
 using namespace std;
 
 class Node {
@@ -7,7 +6,6 @@ public :
     int data;
     Node* right;
     Node* left;
-
     Node(int val){
         data = val;
     };
@@ -17,7 +15,6 @@ class NodeOfStack {
     public:
     Node* data;
     NodeOfStack* next;
-
     NodeOfStack(Node* node){
         data = node;
     }
@@ -26,27 +23,31 @@ class NodeOfStack {
 class Stack{
 public :
     NodeOfStack* head;
-
+    int length = 0;
     void push(Node* node){
         NodeOfStack* newNode = new NodeOfStack(node);
         if(head == NULL){
             head = newNode;
+            length++;
         }else{
             newNode->next = head;
             head = newNode;
+            length++;
         }
     }
 
-    void pop(){
+    NodeOfStack* pop(){
         if(head == NULL){
-            return;
+            return NULL;
         }else{
+            NodeOfStack* removedNode = head;
             head = head->next;
+            length--;
+            return removedNode;
         }
     }
 
     void printStack(){
-
         NodeOfStack* currNode = head;
         cout << "stack data print starts" <<endl;
         while (currNode->next != NULL)
@@ -57,24 +58,109 @@ public :
         cout << currNode->data->data <<endl;
         cout << "stack data print ends" <<endl;
         return;
-        
     }
-
 };
 
-void printAll(Node* root){
+
+void depthFirstTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+    Stack* stack1 = new Stack();
+    stack1->push(root);
+    cout << "print starts for depth first traversal method" << endl;
+    while(stack1->length > 0){
+        const NodeOfStack* current = stack1->pop();
+        cout << current->data->data <<endl ;
+        if(current->data->right != NULL){
+            stack1->push(current->data->right);
+        }if(current->data->left != NULL){
+            stack1->push(current->data->left);
+        }
+    }
+    cout << "print ends for depth first traversal method" << endl;
+}
+
+//---------------------
+
+class NodeOfQueue {
+    public :
+    Node* data;
+    NodeOfQueue* next;
+    NodeOfQueue(Node* node){
+        data = node;
+    }
+};
+
+class Queue{
+    public :
+    NodeOfQueue* head;
+    int length = 0;
+    
+    void push(Node* node){
+        NodeOfQueue* newNode = new NodeOfQueue(node);
+        if(head==NULL){
+            head = newNode;
+            length++;
+        }else{
+            NodeOfQueue* currNode = head;
+            while (currNode->next != NULL) {
+                currNode = currNode->next;
+            }
+            currNode->next = newNode;
+            length++;
+        }
+    }
+    
+    NodeOfQueue* pop(){
+        if(head==NULL){
+            return NULL;
+        }else{
+            NodeOfQueue* removedNode = head;
+            head = head->next;
+            length--;
+            return removedNode;
+        }
+    }
+    
+    void print(){
+        NodeOfQueue* currNode = head;
+        cout << "queue print starts" <<endl;
+        while (currNode->next !=NULL) {
+            cout << currNode->data->data <<endl;
+            currNode = currNode->next;
+        }
+        cout << currNode->data->data <<endl;
+        cout << "queue print ends" << endl;
+    }
+};
+
+void breadthFirstTraversal(Node* node){
+    Queue* queue1 = new Queue();
+    queue1->push(node);
+    cout << "print for breadth first traversal starts" <<endl;
+    while (queue1->length > 0) {
+        NodeOfQueue* currentNode = queue1->pop();
+        cout << currentNode->data->data <<endl;
+        if(currentNode->data->left != NULL){
+            queue1->push(currentNode->data->left);
+        }if(currentNode->data->right != NULL){
+            queue1->push(currentNode->data->right);
+        }
+    }
+    cout << "print for breadth first traversal ends" <<endl;
+}
+
+
+void depthFirstTraversalByRecursion(Node* root){
     if(root==NULL){
         return;
     }
     cout << root->data << endl;
-    printAll(root->right);
-    printAll(root->left);
+    depthFirstTraversalByRecursion(root->left);
+    depthFirstTraversalByRecursion(root->right);
 }
 
-
-void printByIteration(Node* root){
-    if(root == NULL) return;
-}
 
 int main (){
 
@@ -85,24 +171,17 @@ int main (){
     root->right->left = new Node(40);
     root->left->right = new Node(15);
     root->left->left = new Node(7);
-    cout << "hello" <<endl;
-    // printAll(root);
 
-    // cout << "iteration method : " <<endl;
-
-    // printByIteration(root);
 //         20
 //       /    \
 //     10     44
 //    /  \   /  \
 //   7   15 40  55
+    
+    cout << "depth first traversal method using recursion starts" << endl;
+    depthFirstTraversalByRecursion(root);
+    cout << "depth first traversal method using recursion ends" << endl;
+    depthFirstTraversal(root);
+    breadthFirstTraversal(root);
+}
 
-Stack* stack1 = new Stack();
-
-    stack1->push(root);
-    stack1->push(root->right);
-    stack1->push(root->left);
-
-    stack1->printStack();
-
-}   
